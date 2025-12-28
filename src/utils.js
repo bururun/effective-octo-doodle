56d670b6cacc10717b94450bea5f21c6aab5d649
bururun <bururun@users.noreply.github.com>
2025-12-28 08:34:40 +0200

    add: update tests

--- src/utils.js
+++ src/utils.js
@@ -20,3 +20,27 @@ function newFunction1() {
 }
 
 module.exports = { formatData, validateInput };
+
+
+// Utility functions for DoodleAPI
+
+function formatData(data) {
+    if (typeof data === 'string') {
+        return data.trim().toUpperCase();
+    }
+    return data;
+}
+
+function validateInput(value) {
+    if (value === null || value === undefined) {
+        throw new Error('Value cannot be null');
+    }
+    return true;
+}
+
+// Update 2
+function newFunction2() {
+    return 2;
+}
+
+module.exports = { formatData, validateInput };
