bda3234fc7b0c33beeaceac5deccc6b586780ceb
bururun <bururun@users.noreply.github.com>
2026-01-19 02:24:49 +0200

    test(db): improve user feedback

--- src/index.js
+++ src/index.js
@@ -21,3 +21,28 @@ class DoodleAPI {
 
 // Update 7
 module.exports = DoodleAPI;
+
+
+// Main entry point for DoodleAPI
+
+class DoodleAPI {
+    constructor() {
+        this.initialized = true;
+        this.version = '1.0.8';
+    }
+    
+    start() {
+        return 'Started';
+    }
+    
+    stop() {
+        return 'Stopped';
+    }
+    
+    getStatus() {
+        return { status: 'running', version: this.version };
+    }
+}
+
+// Update 8
+module.exports = DoodleAPI;
